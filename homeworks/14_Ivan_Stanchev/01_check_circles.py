12eacf44bb51009e2c36b894ea27a7cc485d5b2e
IvanStanchev <48806861+IvanStanchev@users.noreply.github.com>
2021-10-18 01:17:19 +0300

    minor fixes
    /

--- homeworks/14_Ivan_Stanchev/01_check_circles.py
+++ homeworks/14_Ivan_Stanchev/01_check_circles.py
@@ -12,22 +12,22 @@ def check_circles(c1_center, c1_radius, c2_center, c2_radius):
 
 # TOUCHING
     if d == sum_of_radii:
-        return "TOUCHING"        
+    	return "TOUCHING"        
     elif d < sum_of_radii:
         
 # CONTAINING
         if d + c2_radius <= c1_radius:
-            return "First circle contains second one"
+        	return "First circle contains second one"
         
         elif d + c1_radius <= c2_radius:
-			return "Second circle contains first one"
+		return "Second circle contains first one"
         
 # INTERSECTING
         else:
-			return "INTERSECTING"
+		return "INTERSECTING"
 # NO_COMMON
     elif d > sum_of_radii:
-		return "NO_COMMON"
+	return "NO_COMMON"
             
 	elif d < sum_of_radii:
 		if d + c2_radius <= c1_radius:
