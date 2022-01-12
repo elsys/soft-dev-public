8353a196074d8eedc992e1f1f1aaa0a558e6a99b
Squidfishrl <ivo.genchev4@gmail.com>
2022-01-13 01:05:33 +0200

    refactor comments

--- homeworks/13_Ivaylo_Genchev/02_reach_ladder_position.py
+++ homeworks/13_Ivaylo_Genchev/02_reach_ladder_position.py
@@ -1,7 +1,7 @@
 from inspect import get_annotations  # type check function arguments
-from sys import (
-    version_info,
-)  # check python version (differt ways to type check annotations)
+
+# check python version (differt ways to type check annotations)
+from sys import version_info
 
 """
 Дадена е стълба с N >= 2 стъпала. Стоим в началото на стълбата и можем да качим 1 или 2 стъпала наведнъж. 
@@ -24,23 +24,20 @@ def num_ways(steps: int) -> int:
         )
 
     if version_info[1] < 10:  # get minor python version (for type checking)
-        if (
-            type(steps) != num_ways.__annotations__["steps"]
-        ):  # PEP compliant way to access annotations before 3.10
+        # PEP compliant way to access annotations before 3.10
+        if type(steps) != num_ways.__annotations__["steps"]:
             raise TypeError(f"Argument 'steps' cannot be of type {type(steps)}.")
     else:  # 3.10+
-        if (
-            type(steps) != get_annotations(num_ways)["steps"]
-        ):  # PEP compliant way to access annotatinos (3.10+)
+        # PEP compliant way to access annotatinos (3.10+)
+        if type(steps) != get_annotations(num_ways)["steps"]:
             raise TypeError(f"Argument 'steps' cannot be of type {type(steps)}.")
 
     def calculate_ways(remaining_steps: int, ways_amount: int = 0) -> int:
         """Recursively calculate the amount of ways N steps can be climbed."""
 
         # exit condition
-        if (
-            remaining_steps < 0
-        ):  # don't change ways_amount due invalid movement (1 step was remaining but 2 were climbed)
+        if remaining_steps < 0:
+            # don't change ways_amount due invalid movement (1 step was remaining but 2 were climbed)
             return ways_amount
         elif remaining_steps == 0:
             return ways_amount + 1
@@ -62,14 +59,12 @@ def fibonacci(n: int) -> int:
 
     # type check
     if version_info[1] < 10:  # get minor python version (for type checking)
-        if (
-            type(n) != fibonacci.__annotations__["n"]
-        ):  # PEP compliant way to access annotations before 3.10
+        if type(n) != fibonacci.__annotations__["n"]:
+            # PEP compliant way to access annotations before 3.10
             raise TypeError(f"Argument 'n' cannot be of type {type(n)}.")
     else:  # 3.10+
-        if (
-            type(n) != get_annotations(fibonacci)["n"]
-        ):  # PEP compliant way to access annotations (3.10+)
+        if type(n) != get_annotations(fibonacci)["n"]:
+            # PEP compliant way to access annotations (3.10+)
             raise TypeError(f"Argument 'n' cannot be of type {type(n)}.")
 
     first: int = 0
