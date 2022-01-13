29454c533952403223b67d38e8a667f07154f816
Ivaelo <62559591+Ivaelo@users.noreply.github.com>
2022-01-13 09:23:30 +0200

    Create zad2.py

---
+++ homeworks/05_Vladimir_Aleksiev/zad2.py
@@ -0,0 +1,20 @@
+
+N = input("Enter how many stairs you will climb: ")
+
+def num_ways(n):
+
+    if n < 0:
+        return 0
+
+    if n == 0:
+        return 1
+
+    nums = [0] * (n+1)
+    nums[0] = 1
+    nums[1] = 2
+
+    for i in range(1, n+1):
+        nums[i] = nums[i-1] + nums[i-2]
+    return nums[n]
+
+print(num_ways(int(N)))
