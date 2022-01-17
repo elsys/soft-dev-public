0af24b3e5dd1c0729e5522bae42a323d30c5c79e
Elena Varbanova <elena.varbanova04@gmail.com>
2022-01-17 15:50:01 +0200

    Update task 3

--- homeworks/09_Elena_Varbanova/replace.py
+++ homeworks/09_Elena_Varbanova/replace.py
@@ -1,25 +1,39 @@
-def replace(list, finding, replacing):
+def replace(listing, finding, replacing):
 	counter = 0
 
-	for index in list:
+	for index in listing:
 
 		if type(index) != int and len(index) > 1:
-			new_list = list[counter]
-			replace(new_list, finding, replacing)
+
+			if type(index) == tuple:
+				new_list = list(listing[counter])
+				new_list = replace(new_list, finding, replacing)
+				listing[counter] = tuple(new_list)
+			
+			else:
+				new_list = listing[counter]
+				new_list = replace(new_list, finding, replacing)
 
 		if index == finding:
-			list[counter] = replacing
+			listing[counter] = replacing
+
 		counter = counter + 1 
 
-	return list
+	return listing
 
 
-list = [ 'a', 1, [ ['a', 'b'], 1], ([1, 3, 'a'], 'b')]
-res = replace(list, 'a', 'c')
+listing = [ 'a', 1, [ ['a', 'b'], 1], ([1, 3, 'a'], 'a')]#last 'a' in English
+res = replace(listing, 'a', 'c')
 print(res)#[ 'c', 1, [ ['c', 'b'], 1], ([1, 3, 'c'], 'b')]
 
 print("...........")
 
-list1 = [ 2, 1, [ ['a', 'b'], 'c'], ([1, 3, 2], 'b')]
-res1 = replace(list1, 1, 'e')
-print(res1)#[ 2, 'e', [ ['a', 'b'], 'c'], (['e', 3, 2], 'b')]
+list1 = [ 'a', 1, [ ['a', 'b'], 1], ([1, 3, 'a'], 'а')]#last 'a' in Bulgarian
+res1 = replace(list1, 'a', 'c')
+print(res1)#[ 2, 'e', [ ['a', 'b'], 'c'], (['e', 3, 2], 'a')]
+
+print("...........")
+
+list2 = [ 2, 1, [ ['a', 'b'], 'c'], ([1, 3, 2], 1)]
+res2 = replace(list2, 1, 'e')
+print(res2)#[ 2, 'e', [ ['a', 'b'], 'c'], (['e', 3, 2], 'b')]
